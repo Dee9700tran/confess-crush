22fa6b4fb145facc1a9ca0dc4ee2d0aad49df61e
Dee9700tran <tranthdung25@gmail.com>
2025-02-09 01:18:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Chóa Dum đã gửi cho bạn 1 lời nhắn",
+    introTitle: 'Đũy Nhi đón xem nhé!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    btnIntro: '^^HiHaHiHo^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    btnYes: 'Chem chép mong manh xíu :v',
+    btnNo: 'Có cái con chem chép :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình nhĩ <3',
+    btnReply: 'Gửi cho bạn hàng ngàn nụ hun mặc dù mình có 2 mòm thui hí hí',
+    reply: 'Yêu thì yêu mà không yêu trả dép bố dzề',
+    mess: 'Mình biết mà 🥰. Mình cũng iu mình lém 😘😘',
+    messDesc: 'Chúc bạng 1 ngày giữa tháng 2 zui zẻ nha! Giá mà được gặm bạn bây giờ nhỉ?.',
+    btnAccept: 'Lại chả thích bome gaaa hahaha',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //https://www.facebook.com/share/p/166BtSeva9/
 }
